8538048babfd8cc580cd6b580d8e653cee4ce02e
Yijun Blue Yuan <Blue.Yuan@morningstar.com>
2015-07-07 23:09:15 +0100

    add AssemblyInfo back

---
+++ ProtoMiddler/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Fiddler;
+
+[assembly: AssemblyTitle("ProtoMiddler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProtoMiddler.Properties")]
+[assembly: AssemblyCopyright("Copyright Jon Boyd jboyd[at]securityinnovation[dot]com © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("fa9141c7-f7e6-4b7b-bda5-d59889587910")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: RequiredVersion("2.3.0.0")]
