81a693c95f0af496d29cc9589e07049ca964c9fe
Yijun Blue Yuan <Blue.Yuan@morningstar.com>
2015-07-08 21:31:14 +0100

    add the folder of input file as include path
    use a flag to control this -- still confused about protoc's behavior

--- ProtoMiddler/ProtoGen/ProtoLoader.cs
+++ ProtoMiddler/ProtoGen/ProtoLoader.cs
@@ -9,18 +9,22 @@ namespace ProtoMiddler.ProtoGen
 {
     class ProtoLoader
     {
+        readonly bool _addToIncludePath;
         readonly ICollection<string> _includePaths;
         readonly List<string> _paths = new List<string>();
 
-        public ProtoLoader(string path, params string[] includePaths)
+        public ProtoLoader(string path, bool addToIncludePath = true, params string[] includePaths)
+            : this(new[] {path}, addToIncludePath, includePaths)
         {
-            _paths.Add(path);
-            _includePaths = includePaths;
         }
 
-        public ProtoLoader(IEnumerable<string> paths, params string[] includePaths)
+        public ProtoLoader(IEnumerable<string> paths, bool addToIncludePath = true, params string[] includePaths)
         {
-            _paths.AddRange(paths);
+            if (paths != null)
+            {
+                _paths.AddRange(paths);
+            }
+            _addToIncludePath = addToIncludePath;
             _includePaths = includePaths;
         }
 
@@ -37,7 +41,10 @@ namespace ProtoMiddler.ProtoGen
         public FileDescriptorSet LoadTypes(Stream ms = null)
         {
             var files = new FileDescriptorSet();
-            var args = string.Join(" ", _includePaths
+            var args = string.Join(" ", (_includePaths ?? new string[0])
+                .Concat(_addToIncludePath
+                    ? _paths.Select(Path.GetDirectoryName)
+                    : new string[0])
                 .Where(Directory.Exists)
                 .Select(o => o.ToLowerInvariant())
                 .Distinct()
